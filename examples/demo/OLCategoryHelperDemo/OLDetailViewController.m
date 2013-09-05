d43093cc0ef9ccfe24be1e2f5b83a90398df6d59
Pay Liu <pay@octalord.com>
2013-09-05 10:07:36 +0800

    demo project of example

---
+++ examples/demo/OLCategoryHelperDemo/OLDetailViewController.m
@@ -0,0 +1,68 @@
+//
+//  OLDetailViewController.m
+//  OLCategoryHelperDemo
+//
+//  Created by Pay Liu on 13/9/5.
+//  Copyright (c) 2013年 Octalord Information Inc. All rights reserved.
+//
+
+#import "OLDetailViewController.h"
+
+@interface OLDetailViewController ()
+- (void)configureView;
+@end
+
+@implementation OLDetailViewController
+
+- (void)dealloc
+{
+    [_detailItem release];
+    [_detailDescriptionLabel release];
+    [super dealloc];
+}
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        [_detailItem release];
+        _detailItem = [newDetailItem retain];
+
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Detail", @"Detail");
+    }
+    return self;
+}
+							
+@end
