d122c7c03e760297592617d16fb88598ce761726
Pay <pay@octalord.com>
2015-06-13 20:27:24 +0800

    head file init

---
+++ src/Category/OLCategoryHelper.h
@@ -0,0 +1,42 @@
+//
+// OLCategoryHelper.h
+// OLCategoryHelper
+//
+// Created by Pay Liu on 2015/6/13.
+// Copyright (c) 2015年 Octalord Information Inc. All rights reserved.
+//
+
+#ifndef OLCategoryHelper_OLCategoryHelper_h
+#define OLCategoryHelper_OLCategoryHelper_h
+
+#import "Math+Degrees.h"
+
+#import "NSData+AES.h"
+
+#import "NSDate+Translator.h"
+
+#import "NSDictionary+JSON.h"
+
+#import "NSNumber+Date.h"
+#import "NSNumber+Random.h"
+
+#import "NSString+Append.h"
+#import "NSString+Blank.h"
+#import "NSString+BOOL.h"
+#import "NSString+Contains.h"
+#import "NSString+InterfaceOrientation.h"
+
+#import "UIColor+HexString.h"
+#import "UIColor+RGB.h"
+
+#import "UIDevice+Enumerator.h"
+#import "UIDevice+Resolutions.h"
+
+#import "UIImage+H568.h"
+
+#import "UINavigationController+OrientationSettings.h"
+#import "UINavigationController+Pilot.h"
+
+#import "UIView+ViewFrameGeometry.h"
+
+#endif
