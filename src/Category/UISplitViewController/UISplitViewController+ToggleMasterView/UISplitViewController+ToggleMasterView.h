c1d5fb625ce703128941917bd8cad971da8a601b
Pay <pay@octalord.com>
2015-09-04 11:33:52 +0800

    UISplitViewController+ToggleMasterView init

---
+++ src/Category/UISplitViewController/UISplitViewController+ToggleMasterView/UISplitViewController+ToggleMasterView.h
@@ -0,0 +1,15 @@
+//
+// UISplitViewController+ToggleMasterView.h
+// OLCategoryHelper
+//
+// Created by Pay Liu on 9/4/15.
+// Copyright (c) 2015 Octalord Information Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISplitViewController (ToggleMasterView)
+
+- (void) toggleMasterView;
+
+@end
